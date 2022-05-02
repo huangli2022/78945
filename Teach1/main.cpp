c2777915ec92f366289b8881a292d98609b78138
zhangpf <809151882@qq.com>
2022-05-02 17:19:15 +0800

    课程1

--- Teach1/main.cpp
+++ Teach1/main.cpp
@@ -4,7 +4,9 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	QPushButton* btn = new QPushButton("hello Qt");
+	QPushButton* btn = new QPushButton("hello Qt", nullptr);
 	btn->show();
+//	QObject::connect(btn, SIGNAL(clicked()), &a, SLOT(quit())); // qt4
+	QObject::connect(btn, &QPushButton::clicked, &a, &QApplication::quit);//qt5
 	return a.exec();
 }
